4bbff424b079718e4d5e7cf3a74479c5e87611df
Samuel Marafigo <swmarafigo@gmail.com>
2024-11-07 01:12:32 -0300

    Fix API url in unitService.ts

--- src/services/unitService.ts
+++ src/services/unitService.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { Unit } from '../types/Unit';
 
+const API_URL = "https://flask-app-rough-glitter-6700.fly.dev"
+
 export const fetchUnits = async (): Promise<Unit[]> => {
-  const response = await axios.get('api/units');
+  const response = await axios.get(`${API_URL}/units`);
   return response.data;
 };
 
 export const fetchUnitStock = async (id: string) => {
-  const response = await axios.get(`api/unit/lookup/${id}`);
+  const response = await axios.get(`${API_URL}/unit/lookup/${id}`);
   return response.data;
 };
