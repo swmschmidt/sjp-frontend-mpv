d6a42353f69921694261de1eda80c7d8489a9767
Samuel Marafigo <swmarafigo@gmail.com>
2024-12-11 16:21:20 -0300

    Change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import OrdersPage from './pages/OrdersPage';
 import UnitDetailsPage from './pages/UnitDetailsPage';
