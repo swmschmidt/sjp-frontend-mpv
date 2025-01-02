0600f1400efb099e0e2db53fb7b85354feb9ef7b
Samuel Marafigo <swmarafigo@gmail.com>
2025-01-02 16:28:09 -0300

    Fix API url

--- src/components/PageTracker.tsx
+++ src/components/PageTracker.tsx
@@ -5,7 +5,7 @@ const PageTracker = () => {
   useEffect(() => {
     const sendPageView = async () => {
       try {
-        await axios.post('https://flask-app-rough-glitter-6700.fly.dev/page_tracker/track_page', { page: window.location.href });
+        await axios.post('https://flask-app-rough-glitter-6700.fly.dev/track_page', { page: window.location.href });
       } catch (error) {
         console.error('Error sending page view:', error);
       }
