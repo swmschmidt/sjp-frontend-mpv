d59054ffd0a4c01b88da8af46025f4016dda178d
Samuel Marafigo <swmarafigo@gmail.com>
2025-01-02 16:25:26 -0300

    Add page tracker component

---
+++ src/components/PageTracker.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const PageTracker = () => {
+  useEffect(() => {
+    const sendPageView = async () => {
+      try {
+        await axios.post('https://flask-app-rough-glitter-6700.fly.dev/page_tracker/track_page', { page: window.location.href });
+      } catch (error) {
+        console.error('Error sending page view:', error);
+      }
+    };
+
+    sendPageView();
+  }, []);
+
+  return null;
+};
+
+export default PageTracker;
